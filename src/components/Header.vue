217483239b847f27b30081ed361f6b57707511de
liyang <272891021@qq.com>
2024-12-13 22:47:41 +0800

    修改header字体颜色

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <RouterLink class="flex mr-auto ml-4 my-2 cursor-pointer" to="/">
     <img src="../assets/logo.png" class="w-12 mx-auto" />
-    <p class="ml-2 text-base text-gray-500 my-auto">广东第二师范学院电脑义务维修中心</p>
+    <p class="ml-2 text-base text-base-content my-auto">广东第二师范学院电脑义务维修中心</p>
   </RouterLink>
 </template>
